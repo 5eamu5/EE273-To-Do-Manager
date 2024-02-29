4dc1d1e678c9f635ae04bd4b001b7208c19dfe34
Seamus O'Connell <156205382+5eamu5@users.noreply.github.com>
2024-02-29 20:13:21 +0000

    Create taskManager.h
    adds a task manager class. an object is created whenever the user interacts with the class, and then is destroyed when the user saves their changes

---
+++ taskManager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+class Task;
+
+class taskManager
+{
+
+public:
+	taskManager();
+	taskManager(Task t); 
+	~taskManager();
+
+	std::vector<Task> general; 
+	std::vector<Task> pushGeneral();
+
+	Task createTask();
+	Task editTask();
+	Task sortTask(std::vector<Task> vec);
+	Task deleteTask(std::vector<Task> vec);
+
+	//editing func
+	int selectTaskToEdit(std::vector<Task> vec);
+
+	void editName(Task temp);
+	void editDeadline(Task temp);
+	void editReminder(Task temp);
+	void editLocation(Task temp);
+	void editSubject(Task temp);
+	void editNote(Task temp);
+	void editUrgent(Task temp);
+
+private:
+
+};
+
