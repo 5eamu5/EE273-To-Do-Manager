d85251821d6719eb9cb48ca802aa3c98bba5ad1d
Seamus O'Connell <156205382+5eamu5@users.noreply.github.com>
2024-02-29 20:15:57 +0000

    Update consoleUI.cpp
    in line with consoleUI.h

--- consoleUI.cpp
+++ consoleUI.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "task.h"
+#include "task.h" 
+#include "taskManager.h"
 #include "consoleUI.h"
 #include "editTask.h"
-#include "sortTask.h"
+#include "sortTask.h"3
+
 #include "deleteTask.h"
 
 using namespace std;
-std::vector<Task> general;
+taskManager task_manager;
+std::vector<Task> general = task_manager.pushGeneral();  
 
 void contents() {
 
@@ -40,13 +43,13 @@ void contents() {
 	case 0:
 		break;
 	case 1:
-		createTask();
+		createTaskUI();
 		break;
 	case 2:
-		editTask();
+		editTaskUI();
 		break;
 	case 3:
-		deleteTask();
+		deleteTaskUI();
 		break;
 	default:
 		std::cout << "please enter an integer between 1 and 4 to represent the desired function" << endl;
@@ -55,42 +58,16 @@ void contents() {
 			
 }
 
-void createTask() {
-
-	//local variables to store user inputs
-	//used to construct temporary task object
-	//temporary task 'temp' is then added to the global vector 'general'
-	//createTask() can then be called again with fresh variables as the created object is stored in global container
-
-	std::string create_name;
-	int create_deadline;
-	int create_reminder;
-	std::string create_location;
-	std::string create_subject;
-	std::string create_note;
+void createTaskUI() {
 
 	upperDivider();
 
 	std::cout << "-CREATE NEW TASK-" << endl;
 	std::cout << "Please enter the following data about your task: " << endl;
 
-	std::cout << "Name: ";
-	std::cin >> create_name;
-	std::cout << "Deadline: "; 
-	std::cin >> create_deadline;
-	std::cout << "Reminder: "; 
-	std::cin >> create_reminder;
-	std::cout << "Location: "; 
-	std::cin >> create_location;
-	std::cout << "Subject: "; 
-	std::cin >> create_subject;
-	std::cout << "Note: ";
-	std::cin >> create_note; 
-
-	Task temp(create_name, create_deadline, create_reminder, create_location, create_subject, false, create_note);
-	general.push_back(temp); 
-	
+	Task temp = task_manager.createTask(); 
 	std::cout << "\n" << temp.toString() << std::endl;
+
 	std::cout << "Task Created Sucessfully" << endl;
 
 	lowerDivider(); 
@@ -99,147 +76,81 @@ void createTask() {
 }
 
 
-void editTask() {
-
-	int edit_input;
+void editTaskUI() {
 
 	upperDivider();
 
 	std::cout << "-EDIT A TASK-" << endl;
 
-	int index_response = selectTaskToEdit(general);
+	int index_response = task_manager.selectTaskToEdit(general); 
 	Task temp = general[index_response];
 
 	std::cout << "You have selected task: " << temp.getName() << endl;
 	std::cout << "Select an element to edit: " << endl << temp.toString() << endl;	
 
-	lowerDivider();
 
 	std::cin >> edit_input;
+
 
 	//need to catch false inputs here
 	switch (edit_input) {
 		case 1:
-			editName(temp);
+			upperDivider();
+			task_manager.editName(temp, general);
 			returnToEdit(); 
+			lowerDivider();
+
 		case 2:
-			editDeadline(temp);
+			upperDivider();
+			task_manager.editDeadline(temp, general);
 			returnToEdit();
+			lowerDivider();
+
 		case 3:
-			editReminder(temp);
+			upperDivider();
+
+			task_manager.editReminder(temp, general);
 			returnToEdit();
+			lowerDivider();
+
 		case 4:
-			editLocation(temp);
+			upperDivider();
+
+			task_manager.editLocation(temp, general);
 			returnToEdit();
+			lowerDivider();
+
 		case 5:
-			editSubject(temp); 
+			upperDivider();
+
+			task_manager.editSubject(temp, general);
 			returnToEdit();
+			lowerDivider();
+
 		case 6:
-			editNote(temp);
+			upperDivider();
+
+			task_manager.editNote(temp, general);
 			returnToEdit();
+			lowerDivider();
+
 		case 7:
-			editUrgent(temp); 
+			upperDivider();
+			task_manager.editUrgent(temp, general);
 			returnToEdit();
+			lowerDivider();
+
 	}
-}
-
-int selectTaskToEdit(std::vector<Task> general) {
-	int index_response;
-
-	std::cout << "Please select a Task to edit: " << endl;
-
-	for (int i{ 0 }; i < general.size(); i++) {
-		std::cout << "Task " << i << ". " << general[i].getName() << endl;
-	}
-	std::cin >> index_response;
-
-	return index_response;
-}
-
-void editName(Task temp) { 
-	std::string edit_name;
-	
-	upperDivider();
-	std::cout << "Current Name: " << temp.getName() << endl;
-	std::cout << "Enter task new name: ";
-	std::cin >> edit_name;
-	temp.setName(edit_name);
-	std::cout << endl;
-	std::cout << "New Task Name: " << temp.getName();
 	lowerDivider();
-}
-void editDeadline(Task temp) {
-	int edit_deadline;
 
-	upperDivider();
-	std::cout << "Current Deadline: " << temp.getName() << endl;
-	std::cout << "Enter task new Deadline: ";
-	std::cin >> edit_deadline;
-	temp.setDeadline(edit_deadline);
-	std::cout << "New Task Deadline: " << temp.getDeadline();
-	lowerDivider();
 }
-void editReminder(Task temp) {
-	int edit_reminder;
 
-	upperDivider();
-	std::cout << "Current Reminder: " << temp.getName() << endl;
-	std::cout << "Enter task new Reminder: ";
-	std::cin >> edit_reminder;
-	temp.setReminder(edit_reminder);
-	std::cout << "New Task Reminder: " << temp.getReminder();
-	lowerDivider(); 
-}
-void editLocation(Task temp) {
-	std::string edit_location;
-
-	upperDivider();
-	std::cout << "Current Location: " << temp.getName() << endl;
-	std::cout << "Enter task new Location: ";
-	std::cin >> edit_location;
-	temp.setLocation(edit_location);
-	std::cout << "New Task Location: " << temp.getLocation();
-	lowerDivider(); 
-}
-void editSubject(Task temp) {
-	std::string edit_subject;
-
-	upperDivider();
-	std::cout << "Current Subject: " << temp.getName() << endl;
-	std::cout << "Enter task new Subject: ";
-	std::cin >> edit_subject;
-	temp.setSubject(edit_subject);
-	std::cout << "New Task Subject: " << temp.getSubject();
-	lowerDivider();
-}
-void editNote(Task temp) {
-	std::string edit_note;
-	
-	upperDivider();
-	std::cout << "Current Note: " << temp.getName() << endl;
-	std::cout << "Enter task new Note: ";
-	std::cin >> edit_note;
-	temp.setNote(edit_note);
-	std::cout << "New Task Note: " << temp.getNote();
-	lowerDivider(); 
-}
-void editUrgent(Task temp) {
-	bool edit_urgent;
-
-	upperDivider(); 
-	std::cout << "Current Urgent: " << temp.getName() << endl;
-	std::cout << "Enter task new Urgent: ";
-	std::cin >> edit_urgent;
-	temp.setUrgent(edit_urgent); 
-	std::cout << "New Task Urgent: " << temp.getUrgent();
-	lowerDivider(); 
-}
 
 
 //everything above finished
 //nothing below started yet 
 
-void sortTask() {
+void sortTaskUI() {
 
 }
 
@@ -260,7 +171,7 @@ void UIsortUrgent() {
 }
 
 
-void deleteTask() {
+void deleteTaskUI() {
 
 }
 
@@ -309,13 +220,16 @@ void returnToEdit() {
 		std::cin >> return_to_edit;
 	}
 	if (return_to_edit == 'y') {
-		editTask();
+		editTaskUI();
 	}
 	else {
 		contents();
 	}
 }
 void returnToSort() {
+
+}
+void returnToDelete() {
 
 }
 
