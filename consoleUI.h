c08a97543115ac1f911de4b0fed516077417bbeb
Seamus O'Connell <156205382+5eamu5@users.noreply.github.com>
2024-03-05 14:55:53 +0000

    Update consoleUI.h

--- consoleUI.h
+++ consoleUI.h
@@ -27,4 +27,33 @@ void returnToSort();
 void returnToDelete();
 
 void upperDivider();
+void lowerDivider();#pragma once
+
+//main page of UI
+void contents();
+
+//primary branches
+void createTaskUI();
+void editTaskUI();
+void sortTaskUI();
+void deleteTaskUI();
+
+//sorting subtree 
+//this is just the UI part of the sorting, not the algorithm itself
+void UIsortName();
+void UIsortDeadline();
+void UIsortLocation();
+void UIsortSubject();
+void UIsortUrgent();
+
+//delete task subtree
+void selectTaskToDelete();
+
+//other
+void returnToContents();
+void returnToEdit();
+void returnToSort();
+void returnToDelete();
+
+void upperDivider();
 void lowerDivider();
