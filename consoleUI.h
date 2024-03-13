39e7c02b0d04991e541b2b664ea159ac99236913
Seamus O'Connell <156205382+5eamu5@users.noreply.github.com>
2024-03-13 15:54:04 +0000

    Update consoleUI.h
    removed sorting UI's (now done through task manager)
    makes code more atomic as all the UI does is format text and display user functionality

--- consoleUI.h
+++ consoleUI.h
@@ -9,17 +9,6 @@ void editTaskUI();
 void sortTaskUI();
 void deleteTaskUI();
 
-//sorting subtree 
-//this is just the UI part of the sorting, not the algorithm itself
-void UIsortName();
-void UIsortDeadline();
-void UIsortLocation();
-void UIsortSubject();
-void UIsortUrgent();
-
-//delete task subtree
-void selectTaskToDelete();
-
 //other
 void returnToContents();
 void returnToEdit();
