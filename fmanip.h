c6c92b34c71eccff72c940ed52e58e8ecefb1934
Seamus O'Connell <156205382+5eamu5@users.noreply.github.com>
2024-03-19 13:09:34 +0000

    Update fmanip.h
    see fmanip.cpp

--- fmanip.h
+++ fmanip.h
@@ -1,12 +1,11 @@
+
 #pragma once
 using namespace std;
 
 class Task;
 
-void wipeFile(vector<Task>& input);
+void wipeFile(vector<Task>& input, std::string filename);
 
-void savetoFile(std::vector<Task>& input);
+void savetoFile(std::vector<Task>& input, std::string filename);
 
-void readfromFile(std::vector<Task>& input);
-
-void readfromFile(vector<Task>& input, string filename);
+void readfromFile(std::vector<Task>& input, std::string filename);
